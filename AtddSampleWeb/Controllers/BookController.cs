01cd377fe6cbd90c8daf25217a07d4ff0ab54993
hatelove <jay.hatelove@gmail.com>
2017-02-11 01:04:33 +0800

    pass integration test case: BookController query books

--- AtddSampleWeb/Controllers/BookController.cs
+++ AtddSampleWeb/Controllers/BookController.cs
@@ -1,6 +1,8 @@
 ﻿using AtddSampleWeb.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using AtddSampleWeb.Models.DataModels;
 
 namespace AtddSampleWeb.Controllers
 {
@@ -50,11 +52,14 @@ namespace AtddSampleWeb.Controllers
 
         private IEnumerable<BookViewMoel> GetBooks(BookQueryViewModel model)
         {
-            return new List<BookViewMoel>
+            using (var dbcontext = new BookEntities())
             {
-                new BookViewMoel() {Name = "玩出好創意", ISBN = "9789869094481"},
-                new BookViewMoel() {Name = "玩出好創意", ISBN = "9789869094481"},
-            };
+                var books = dbcontext.Books
+                    .Where(x => x.Name == model.Name)
+                    .Select(x => new BookViewMoel() { Name = x.Name, ISBN = x.ISBN }).ToList();
+
+                return books;
+            }
         }
     }
 }
