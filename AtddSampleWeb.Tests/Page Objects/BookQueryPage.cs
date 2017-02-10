5cd54cc8aba6c66289f22c9fdc77d61c7e0f3922
hatelove <jay.hatelove@gmail.com>
2017-02-10 18:01:52 +0800

    completed page object: BookQueryPage

--- AtddSampleWeb.Tests/Page Objects/BookQueryPage.cs
+++ AtddSampleWeb.Tests/Page Objects/BookQueryPage.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentAutomation;
 
 namespace AtddSampleWebTests
@@ -7,17 +9,22 @@ namespace AtddSampleWebTests
     {
         public BookQueryPage(FluentTest test) : base(test)
         {
-
+            this.Url = $"{PageContext.Domain}/books/query";
         }
 
         public void Query(BookQueryCondition condition)
         {
-            throw new System.NotImplementedException();
+            I.Enter(condition.Name).In("#name")
+                .Click("input[type=\"submit\"]");
         }
 
         public void ShouldDisplay(IEnumerable<BookQueryResult> books)
         {
-            throw new System.NotImplementedException();
+            for (int i = 0; i < books.Count(); i++)
+            {
+                var book = books.ElementAt(i);
+                I.Assert.Text(book.Name).In($"td .name:nth-child({i + 1})");
+            }
         }
     }
 }
--- AtddSampleWeb.Tests/Page Objects/DTO/BookQueryResult.cs
+++ AtddSampleWeb.Tests/Page Objects/DTO/BookQueryResult.cs
@@ -2,5 +2,6 @@
 {
     public class BookQueryResult
     {
+        public string Name { get; set; }
     }
 }
